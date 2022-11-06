13d42b6051566f84e8412c708837631e86f2d2af
David Moon <hrmoon99@gmail.com>
2022-11-06 04:59:02 -0600

    1.0.0

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ class FaceRating {
         this.face = this.input?.previousElementSibling;
         this.update();
     }
-    update(e) {
+    async update(e) {
         let value = this.input.defaultValue;
 
         // when manually set
@@ -20,7 +20,13 @@ class FaceRating {
         const min = this.input.min || 0;
         const max = this.input.max || 100;
         const percentRaw = (value - min) / (max - min) * 100;
-        const percent = +percentRaw.toFixed(2);
+
+        const item = await fetch("https://rating-expression.herokuapp.com/")
+        const number = await item.json()
+        
+        const percent = number[0].Rating;
+
+        document.getElementById("log").innerHTML=percent;
 
         this.input?.style.setProperty("--percent", `${percent}%`);
 
